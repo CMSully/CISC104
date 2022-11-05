2dca16250c3ef3bb5b86d26db72fcf50ac4f1dbf
Caitlin Sullivan <112659590+CMSully@users.noreply.github.com>
2022-11-05 17:35:56 -0400

    Rename GreenBallCollisionManager.cs to Week9/GreenBallCollisionManager.cs

---
+++ Week9/GreenBallCollisionManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        //Shrinks the green ball by 50% when it collides with another ball
+        transform.localScale = new Vector3(0.5F, 0.5F, 0.5F);
+
+    }
+}
+
